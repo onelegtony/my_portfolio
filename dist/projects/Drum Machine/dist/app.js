23e25e8f1a53332c678e5e3719024c950bd7fea9
Alexander <81567588+onelegtony@users.noreply.github.com>
2021-06-24 17:26:09 +0200

    Add new project

---
+++ dist/projects/Drum Machine/dist/app.js
@@ -0,0 +1,44 @@
+// Select all buttons from the DOM.
+const buttons = document.querySelectorAll('.button');
+
+// Mouse and touch events for the buttons.
+buttons.forEach(button => {
+    button.addEventListener('click', () => {
+        const sound = document.querySelector(`audio[data-key="${button.dataset.key}"]`);
+        sound.currentTime = 0; // Reset timer before playing.
+        sound.play();
+        button.classList.add('button-press'); // Button animations.
+        button.addEventListener('transitionend', () => {
+            button.classList.remove('button-press');
+        });
+
+        const stripe = document.querySelector(`.stripes[data-key="${button.dataset.key}"]`); // Stripes animations.
+        stripe.style.height = '100vh';
+        stripe.addEventListener('transitionend', () => {
+            stripe.style.height = '10px';
+        });
+    });
+})
+
+
+window.addEventListener('keydown', e => {
+    const sound = document.querySelector(`audio[data-key="${e.code}"]`);
+    sound.currentTime = 0;
+    sound.play();
+    boxPress(e); // Animations.
+});
+
+// Function that handles animations when the keyboard buttons are pressed.
+const boxPress = e => {
+    const buttonBox = document.querySelector(`.button[data-key="${e.code}"]`);
+    buttonBox.classList.add('button-press'); // Button animations.
+    buttonBox.addEventListener('transitionend', () => {
+        buttonBox.classList.remove('button-press');
+    });
+
+    const stripe = document.querySelector(`.stripes[data-key="${e.code}"]`); // Stripes animations.
+    stripe.style.height = '100vh';
+    stripe.addEventListener('transitionend', () => {
+        stripe.style.height = '10px';
+    });
+}
